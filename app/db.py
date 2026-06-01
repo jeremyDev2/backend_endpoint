5e22d2136bd490c615e96405576c28a24a215dfc
Daniel <danil.malaev3@gmail.com>
2026-06-01 22:59:27 +0300

    add: import's for alchemy

---
+++ app/db.py
@@ -0,0 +1,14 @@
+from app.settings import settings
+from sqlalchemy import (
+    BigInteger,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+    UniqueConstraint,
+    func,
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
