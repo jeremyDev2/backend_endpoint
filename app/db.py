80a94be78c1837c33da9563cce7e245e791842bf
Daniel <danil.malaev3@gmail.com>
2026-06-03 22:26:47 +0300

    add: implement Base & Product obj.

--- app/db.py
+++ app/db.py
@@ -1,14 +1,12 @@
 from app.settings import settings
-from sqlalchemy import (
-    BigInteger,
-    DateTime,
-    Enum,
-    ForeignKey,
-    Integer,
-    String,
-    Text,
-    UniqueConstraint,
-    func,
-)
-from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase): pass
+
+class Product(Base):
+    
+    __tablename__ = "products"
+
+    product_id: Mapped[int] = mapped_column(primary_key=True)
+    stock: Mapped[int]
+    description: Mapped[str]
