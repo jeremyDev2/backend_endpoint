683471ce5a7f3518f3694006ca19a5133c962c25
Daniel <danil.malaev3@gmail.com>
2026-06-07 23:30:50 +0300

    add: data frame

---
+++ app/data.py
@@ -0,0 +1,5 @@
+products = [
+    {"product_id": 1, "stock": 100, "description": "Phone"},
+    {"product_id": 2, "stock": 50,  "description": "Laptop"},
+    {"product_id": 3, "stock": 200, "description": "Headphones"},
+]
