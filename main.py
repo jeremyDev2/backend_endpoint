eccd819a6fbdaad3ea47e3b194493e57b4838488
Daniel <danil.malaev3@gmail.com>
2026-06-14 17:45:11 +0300

    update: implementation of main. Adding session factory and redis pipeline

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
-from app.session import engine
-from app.db import Base
 from contextlib import asynccontextmanager
+from sqlalchemy import select
+from app.session import session_factory
+from app.db import Product
+from app.cache.redis_client import redis_client
+from app.router import router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    async with session_factory() as session:
+        res = await session.execute(select(Product))
+        products = res.scalars().all()
+
+    async with redis_client.pipeline() as pipe:
+        for product in products:
+            pipe.set(f"stock:{product.product_id}", product.stock)
+        await pipe.execute()
     yield
 
-app = FastAPI(lifespan=lifespan) 
-
-
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
