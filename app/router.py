94cae81caded0414465436d9494df0248c5dd595
Daniel <danil.malaev3@gmail.com>
2026-06-14 17:42:53 +0300

    update: router implementation with custom exceptions

--- app/router.py
+++ app/router.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
 from app.schemas import PurchaseRequest, PurchaseResponse
 from app.db import repository
-from app.exceptions.exceptions import NotEnoughStock
+from app.exceptions.exceptions import NotEnoughStock, ProductNotFound
 
 router = APIRouter()
 
 @router.post("/purchase")
-async def purchase(request:PurchaseRequest) -> PurchaseResponse:
-    success:bool = await repository.purchase(request)
-    if not success:
+async def purchase(request:PurchaseRequest) -> PurchaseResponse: 
+    res: int = await repository.purchase_redis(request)
+
+    if res == -2:
+        raise ProductNotFound(request.product_id)
+    if res == -1:
         raise NotEnoughStock(stock=0, requested=request.purchased_count)
+
     return PurchaseResponse(status="success")
