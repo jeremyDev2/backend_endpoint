8e5f7dadd9e361f578ea5abfbd4af61fe6c1aa8e
Daniel <danil.malaev3@gmail.com>
2026-06-14 17:49:55 +0300

    add: task to Celery worker

--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -1,4 +1,4 @@
 from celery import Celery
 from app.settings import settings
 
-celery_app = Celery("worker", broker=str(settings.amqp_dns))
+celery_app = Celery("worker", broker=str(settings.amqp_dns), include=["app.worker.task"])
