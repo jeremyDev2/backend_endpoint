52702ce8cebcf60aa6b9158a5bbd17bc4012d7cf
Daniel <danil.malaev3@gmail.com>
2026-06-15 21:35:21 +0300

    update: implementation with persistent event loop

--- app/worker/task.py
+++ app/worker/task.py
@@ -3,10 +3,24 @@ from app.worker.celery_app import celery_app
 from sqlalchemy import update
 from app.session import session_factory
 from app.db import Product
+import threading
+from celery.signals import worker_process_init, worker_process_shutdown
+
+loop = asyncio.new_event_loop()
+
+@worker_process_init.connect
+def __start_loop(**kwargs):
+    threading.Thread(target=loop.run_forever, daemon=True).start()
+
+@worker_process_shutdown.connect
+def stop_loop(**kwargs):
+    loop.call_soon_threadsafe(loop.stop)
 
 @celery_app.task(name="sync_stock_to_db")
 def sync_stock_to_db(product_id: int, purchased_count: int) -> None:
-    asyncio.run(_sync(product_id, purchased_count))
+    future = asyncio.run_coroutine_threadsafe(_sync(product_id, purchased_count), loop=loop)
+    return future.result()
+
 
 async def _sync(product_id: int, purchased_count: int) -> None:
     async with session_factory() as session:
