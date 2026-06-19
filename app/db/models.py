ac710fe8c99cf1301ca9e76a7e3de28d5e53068e
Danial <114513984+jeremyDev2@users.noreply.github.com>
2026-06-19 12:40:19 +0300

    Delete comment's
    Removed comments explaining the use of Postgres and Redis.

--- app/db/models.py
+++ app/db/models.py
@@ -2,9 +2,6 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase): pass
 
-# Postgres — постоянное хранилище остатков товаров.
-# Во время работы приложения актуальный счётчик хранится в Redis;
-# Postgres обновляется асинхронно Celery-воркером после каждой успешной покупки.
 class Product(Base):
     __tablename__ = "products"
 
